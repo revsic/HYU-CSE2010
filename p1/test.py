78302d4c938bd0c21dac54ff28290043fbcc2b6a
revsic <revsic99@gmail.com>
2019-03-08 15:36:13 +0900

    p1_2: Add tests

--- p1/test.py
+++ p1/test.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# p1_1 selection sort test
 for fname in ['1_1test1.txt', '1_1test2.txt', '1_1test3.txt']:
     os.rename(fname, 'input.txt')
     os.system('./p1_1')
@@ -21,4 +22,19 @@ for fname in ['1_1test1.txt', '1_1test2.txt', '1_1test3.txt']:
     os.rename('input.txt', fname)
 
 print('p1_1 success')
+
+# p1_2 test
+os.system('./p1_2 student_information.txt output.txt')
+with open('student_information.txt') as f:
+    data = f.read()
+    predicted = '\n'.join(data.split('\n')[1:])
+
+with open('output.txt') as f:
+    output = f.read()
+
+if predicted != output:
+    print('error occured from p1_2')
+    sys.exit(1)
+
+print('p1_2 success')
 sys.exit(0)
