4dbea8b42b6084f63e950521bae918e52b015e7e
revsic <revsic99@gmail.com>
2019-03-21 17:14:53 +0900

    lab3-3: Add description

--- lab3-3/p3-3.c
+++ lab3-3/p3-3.c
@@ -2,11 +2,13 @@
 
 #define MAX_BUFFER_SIZE 100
 
+// Struct for Stack data structure implementation.
 typedef struct {
     int idx;
     int buffer[MAX_BUFFER_SIZE];
 } Stack;
 
+// Generate empty stack. Initialize with start index 0.
 Stack empty_stack() {
     Stack stack;
     stack.idx = 0;
@@ -14,6 +16,12 @@ Stack empty_stack() {
     return stack;
 }
 
+// Push `data` into `stack`.
+// Returns:
+//     1 for success.
+//     0 for failure.
+// Failure:
+//     Try pushing data to full stack.
 int push(Stack* stack, int data) {
     if (stack->idx >= MAX_BUFFER_SIZE) {
         return 0;
@@ -22,6 +30,12 @@ int push(Stack* stack, int data) {
     return 1;
 }
 
+// Pop element from `stack` and store it to `res`.
+// Returns:
+//     1 for success.
+//     0 for failure.
+// Failure:
+//     Try popping from empty stack.
 int pop(Stack* stack, int* res) {
     if (stack->idx < 1) {
         return 0;
@@ -35,14 +49,21 @@ int pop(Stack* stack, int* res) {
     return 1;
 }
 
+// Validate if stack is empty.
 int empty(Stack* stack) {
     return stack->idx < 1;
 }
 
+// Get top element from stack.
 int top(Stack* stack) {
     return stack->buffer[stack->idx - 1];
 }
 
+// Get precedence from given operator.
+// Returns:
+//     2 for *, /, %
+//     1 for +, -
+//     0 for otherwise
 int prec(char oper) {
     switch (oper) {
     case '*':
@@ -57,6 +78,7 @@ int prec(char oper) {
     }
 }
 
+// Calculate operator with given operands.
 int operate(char oper, int n1, int n2) {
     switch (oper) {
     case '*':
@@ -74,6 +96,9 @@ int operate(char oper, int n1, int n2) {
     }
 }
 
+// Convert infix form `input` to postfix form `output`.
+// Condition:
+//     #-terminated string `input`
 int make_postfix(char* output, char* input) {
     int res;
 
@@ -82,24 +107,34 @@ int make_postfix(char* output, char* input) {
 
     Stack oper_stack = empty_stack();
 
+    // #-terminated string
     while (input[++i] != '#') {
+        // If digit
         if (input[i] >= '0' && input[i] <= '9') {
             output[idx++] = input[i];
         }
+        // If open parenthesis
         else if (input[i] == '(') {
             push(&oper_stack, '(');
         }
+        // If close parenthesis
         else if (input[i] == ')') {
+            // Until finding open parenthesis
             while (top(&oper_stack) != '(') {
                 output[idx++] = top(&oper_stack);
                 pop(&oper_stack, NULL);
             }
+            // Pop open parenthesis
             pop(&oper_stack, NULL);
         }
+        // If stack is empty
+        // If precedence of top operator is lower than current operator.
         else if (empty(&oper_stack) || prec(top(&oper_stack)) < prec(input[i])) {
             push(&oper_stack, input[i]);
         }
+        // If precedence of top operator is higher than current operator.
         else {
+            // Until lower precedence operator appears 
             while (prec(top(&oper_stack)) >= prec(input[i])) {
                 pop(&oper_stack, &res);
                 output[idx++] = res;
@@ -108,6 +143,7 @@ int make_postfix(char* output, char* input) {
         }
     }
 
+    // Clear operator stack
     while (!empty(&oper_stack)) {
         pop(&oper_stack, &res);
         output[idx++] = res;
@@ -116,14 +152,17 @@ int make_postfix(char* output, char* input) {
     return idx;
 }
 
+// Calculate postfix form `input`.
 int calc_postfix(char* input, int len) {
     Stack num_stack = empty_stack();
 
     int i, n1, n2;
     for (i = 0; i < len; ++i) {
+        // If digit
         if (input[i] >= '0' && input[i] <= '9') {
             push(&num_stack, input[i] - '0');
         }
+        // If operator
         else {
             pop(&num_stack, &n2);
             pop(&num_stack, &n1);
@@ -135,21 +174,26 @@ int calc_postfix(char* input, int len) {
 }
 
 int main() {
+    // Prepare for file I/O.
     FILE* input = fopen("input.txt", "r");
     FILE* output = fopen("output.txt", "w");
 
     char infix[MAX_BUFFER_SIZE + 1] = { 0, };
     fscanf(input, "%s", infix);
 
+    // Convert to postfix form.
     char postfix[MAX_BUFFER_SIZE + 1] = { 0, };
     int size = make_postfix(postfix, infix);
 
+    // Calculate postfix form.
     int result = calc_postfix(postfix, size);
 
+    // Convert #-terminated string to NULL-terminated string
     int idx = -1;
     while (infix[++idx] != '#');
     infix[idx] = 0;
 
+    // Log
     fprintf(output, "Infix Form : %s\n", infix);
     fprintf(output, "Postfix Form : %s\n", postfix);
     fprintf(output, "Evaluation Result : %d\n", result);
