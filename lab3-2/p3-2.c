e14650e71a0a626c9dc248462e3807d6d83d3350
revsic <revsic99@gmail.com>
2019-03-21 17:06:20 +0900

    lab3-2: Add description

--- lab3-2/p3-2.c
+++ lab3-2/p3-2.c
@@ -3,12 +3,14 @@
 
 #define MAX_QUEUE_SIZE 100
 
+// Struct for Queue data structure implementation.
 typedef struct {
     int size;
     int head, tail;
     int buffer[MAX_QUEUE_SIZE];
 } Queue;
 
+// Generate empty queue. Initialize with size 0.
 Queue empty_queue() {
     Queue queue;
     queue.size = 0;
@@ -18,6 +20,12 @@ Queue empty_queue() {
     return queue;
 }
 
+// Enqueue `data` into `queue`.
+// Returns:
+//     1 for success.
+//     0 for failure.
+// Failure:
+//     Try appending data to full queue.
 int enqueue(Queue* queue, int data) {
     if (queue->size > MAX_QUEUE_SIZE) {
         return 0;
@@ -29,6 +37,12 @@ int enqueue(Queue* queue, int data) {
     return 1;
 }
 
+// Dequeue element from `queue` and store it to `res`.
+// Returns:
+//     1 for success.
+//     0 for failure.
+// Failure:
+//     Try dequeueing data from empty queue.
 int dequeue(Queue* queue, int* res) {
     if (queue->size < 1) {
         return 0;
@@ -41,6 +55,7 @@ int dequeue(Queue* queue, int* res) {
 }
 
 int main() {
+    // Prepare for file I/O.
     FILE* input = fopen("input.txt", "r");
     FILE* output = fopen("output.txt", "w");
 
@@ -50,23 +65,31 @@ int main() {
     int num, res;
     char option[10] = { 0, };
 
+    // Generate empty queue.
     Queue queue = empty_queue();
 
     for (int i = 0; i < n_query; ++i) {
         fscanf(input, "%s", option);
 
+        // Enqueue proper data to queue.
         if (!strcmp(option, "enQ")) {
             fscanf(input, "%d", &num);
             res = enqueue(&queue, num);
+
+            // If failure (Full of queue)
             if (res == 0) {
                 fprintf(output, "Full\n");
             }
         }
+        // Dequeue proper data from queue.
         else if (!strcmp(option, "deQ")) {
             res = dequeue(&queue, &num);
+
+            // If failure (Empty of queue)
             if (res == 0) {
                 fprintf(output, "Empty\n");
             }
+            // If success
             else {
                 fprintf(output, "%d\n", num);
             }
