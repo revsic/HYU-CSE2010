99e25e6aa6da79d1d5f48998fb2f8488cbeb8879
revsic <revsic99@gmail.com>
2019-03-28 22:09:01 +0900

    lab4: Add annotations

--- lab4/p4.c
+++ lab4/p4.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Macro for left or right direction of binary tree
 #define LEFT 0
 #define RIGHT 1
 
+// Macro for getter option or setter option for function `threaded`
 #define GET 0
 #define SET 1
 
+// type alias for byte type
 typedef unsigned char byte;
 
+// Threaded Tree
 typedef struct ThreadedTree_ {
     byte thread;
     char data;
@@ -16,36 +20,43 @@ typedef struct ThreadedTree_ {
     struct ThreadedTree_* right;
 } ThreadedTree;
 
+// Linked list for structure Stack
 typedef struct List_ {
     ThreadedTree* tree;
     struct List_* next;
 } List;
 
+// Stack structure with linked list
 typedef struct {
     List* top;
 } Stack;
 
+// Pair object for finding insertion point
 typedef struct {
     ThreadedTree* tree;
     ThreadedTree* parent;
 } TreePair;
 
+// Linked list for structure Queue
 typedef struct PairList_ {
     TreePair pair;
     struct PairList_* next;
 } PairList;
 
+// Queue structure with linked list
 typedef struct {
     PairList* front;
     PairList* back;
 } Queue;
 
+// Generate empty stack
 Stack make_stack() {
     Stack stack;
     stack.top = NULL;
     return stack;
 }
 
+// Delete stack
 void delete_stack(Stack* stack) {
     List* next;
     while (stack->top) {
@@ -55,6 +66,7 @@ void delete_stack(Stack* stack) {
     }
 }
 
+// Push object to stack
 void push(Stack* stack, ThreadedTree* tree) {
     List* head = malloc(sizeof(List));
     head->next = stack->top;
@@ -62,6 +74,10 @@ void push(Stack* stack, ThreadedTree* tree) {
     stack->top = head;
 }
 
+// Pop object from stack
+// Returns:
+//     NULL, if stack is empty
+//     ThreadedTree*, if otherwise
 ThreadedTree* pop(Stack* stack) {
     if (stack->top == NULL) {
         return NULL;
@@ -75,10 +91,14 @@ ThreadedTree* pop(Stack* stack) {
     return data;
 }
 
+// Get top object of stack.
+// Exceptions:
+//     if stack is empty
 ThreadedTree* top(Stack* stack) {
     return stack->top->tree;
 }
 
+// Generate empty TreePair
 TreePair empty_tree_pair() {
     TreePair pair;
     pair.tree = NULL;
@@ -86,6 +106,7 @@ TreePair empty_tree_pair() {
     return pair;
 }
 
+// Generate empty queue
 Queue make_queue() {
     Queue queue;
     queue.back = NULL;
@@ -93,6 +114,7 @@ Queue make_queue() {
     return queue;
 }
 
+// Delete queue
 void delete_queue(Queue* queue) {
     PairList* pair;
     while (queue->front != queue->back) {
@@ -106,6 +128,7 @@ void delete_queue(Queue* queue) {
     }
 }
 
+// Enqueue object to queue
 void push_back(Queue* queue, ThreadedTree* tree, ThreadedTree* parent) {
     PairList* list = malloc(sizeof(PairList));
     list->next = NULL;
@@ -120,6 +143,10 @@ void push_back(Queue* queue, ThreadedTree* tree, ThreadedTree* parent) {
     }
 }
 
+// Dequeue object from queue
+// Returns:
+//     empty_tree_pair(), if queue is empty
+//     TreePair, if otherwise
 TreePair pop_front(Queue* queue) {
     PairList* front = queue->front;
     if (front == NULL) {
@@ -136,10 +163,22 @@ TreePair pop_front(Queue* queue) {
     return ret;
 }
 
+// Get front object of queue
+// Exceptions:
+//     if queue is empty
 TreePair front(Queue* queue) {
     return queue->front->pair;
 }
 
+// Querying thread pointer from given tree
+// Args:
+//     tree: ThreadedTree*, node
+//     dir: LEFT or RIGHT, querying direction
+//     set: GET or SET, querying method
+// Returns:
+//     1, if LEFT is set
+//     2, if RIGHT is set
+//     0, if given node doesn't have thread pointer
 int threaded(ThreadedTree* tree, int dir, int set) {
     if (set == SET) {
         tree->thread |= (1 << dir);
@@ -147,6 +186,7 @@ int threaded(ThreadedTree* tree, int dir, int set) {
     return tree->thread & (1 << dir);
 }
 
+// Generate empty tree
 ThreadedTree* make_tree(char data) {
     ThreadedTree* tree = malloc(sizeof(ThreadedTree));
     tree->thread = 0;
@@ -156,6 +196,7 @@ ThreadedTree* make_tree(char data) {
     return tree;
 }
 
+// Delete all tree nodes
 void delete_tree(ThreadedTree* root) {
     Stack stack = make_stack();
     push(&stack, root);
@@ -175,10 +216,12 @@ void delete_tree(ThreadedTree* root) {
     delete_stack(&stack);
 }
 
+// Find insertion point, targeting full-complete binary tree
 ThreadedTree** insert_point(ThreadedTree* tree) {
     Queue queue = make_queue();
     push_back(&queue, tree, NULL);
-    
+
+    // Level order traverse to find insertion point
     TreePair pair;
     while (1) {
         pair = pop_front(&queue);
@@ -199,15 +242,19 @@ ThreadedTree** insert_point(ThreadedTree* tree) {
     }
 }
 
+// Insert node with given data
 void insert_node(ThreadedTree* tree, char data) {
     ThreadedTree** node = insert_point(tree);
     *node = make_tree(data);
 }
 
+// Generate inorder thread pointer
 void make_inorder_threaded(ThreadedTree* node) {
     int already_pop = 0;
     ThreadedTree* prev = NULL;
     Stack stack = make_stack();
+
+    // In-order traverse
     while (1) {
         if (already_pop) {
             already_pop = 0;
@@ -239,6 +286,7 @@ void make_inorder_threaded(ThreadedTree* node) {
     delete_stack(&stack);
 }
 
+// Get next node based on thread pointer
 ThreadedTree* next_node(ThreadedTree* node) {
     ThreadedTree* tmp = node->right;
     if (!threaded(node, RIGHT, GET)) {
@@ -249,6 +297,7 @@ ThreadedTree* next_node(ThreadedTree* node) {
     return tmp;
 }
 
+// Traverse tree in in-order and run given callback with proper data
 void traverse(ThreadedTree* node, void(*func)(char)) {
     while (!threaded(node, LEFT, GET)) {
         node = node->left;
@@ -263,38 +312,46 @@ void traverse(ThreadedTree* node, void(*func)(char)) {
     }
 }
 
+// Printer callback
 FILE* output;
 void print(char data) {
     fprintf(output, "%c  ", data);
 }
 
 int main() {
+    // Preparing File I/O
     output = fopen("output.txt", "w");
     FILE* input = fopen("input.txt", "r");
 
+    // Get size of list
     int n_list;
     fscanf(input, "%d", &n_list);
-    fgetc(input);
 
     int i;
     char data[2];
     ThreadedTree* root;
 
     if (n_list > 0) {
+        // Generate root node
         fscanf(input, "%s", data);
         root = make_tree(data[0]);
 
+        // Insert node
         for (i = 1; i < n_list; ++i) {
             fscanf(input, "%s", data);
             insert_node(root, data[0]);
         }
 
+        // Make thread pointer in in-order
         make_inorder_threaded(root);
+        // Traverse tree with callback `print`
         traverse(root, print);
 
+        // Delete tree
         delete_tree(root);
     }
 
+    // Delete file objects
     fclose(input);
     fclose(output);
     return 0;
